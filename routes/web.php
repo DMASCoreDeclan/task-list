8c6c415610077e2f0c483ee90216b2e62e42e03a
Declan Sweeney <declan.sweeney@gmail.com>
2025-03-21 18:27:25 +0000

    Lesson 11 - All about routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get("/hello", function () {
+    return 'Hello';
+});
+
+Route::get('/greet/{name}', function ($name)  {
+    return 'Hello ' .  $name . '!';
+
 });
